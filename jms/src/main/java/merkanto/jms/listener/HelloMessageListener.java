15d0feed83710b3e8ec7a5a6a03c50530e59bdae
Merkanto <miroslav.i.zhelezchev@gmail.com>
2022-07-19 16:59:57 +0300

    Adding default user and pass.

--- jms/src/main/java/merkanto/jms/listener/HelloMessageListener.java
+++ jms/src/main/java/merkanto/jms/listener/HelloMessageListener.java
@@ -32,7 +32,8 @@ public class HelloMessageListener {
 
     @JmsListener(destination = JmsConfig.MY_SEND_RCV_QUEUE)
     public void listenForHello(@Payload HelloWorldMessage helloWorldMessage,
-                               @Headers MessageHeaders headers, Message message) throws JMSException {
+                               @Headers MessageHeaders headers, Message jmsMessage,
+                               org.springframework.messaging.Message springMessage) throws JMSException {
 
         HelloWorldMessage payloadMsg = HelloWorldMessage
                 .builder()
@@ -40,7 +41,7 @@ public class HelloMessageListener {
                 .message("World!")
                 .build();
 
-        jmsTemplate.convertAndSend(message.getJMSReplyTo(), payloadMsg);
+        jmsTemplate.convertAndSend(jmsMessage.getJMSReplyTo(), payloadMsg);
 
 //        throw new RuntimeException("foo");
     }
