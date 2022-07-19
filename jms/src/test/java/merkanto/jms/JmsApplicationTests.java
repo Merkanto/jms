62555e58bf33118053ad52ced8a6cf5f7f7d6171
Merkanto <miroslav.i.zhelezchev@gmail.com>
2022-07-19 14:33:51 +0300

    Init commit.

---
+++ jms/src/test/java/merkanto/jms/JmsApplicationTests.java
@@ -0,0 +1,13 @@
+package merkanto.jms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
